f1d0936e242a98a9df6b927c62e529c5265a0f45
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2021-12-30 17:02:27 +0300

    Добавьте файлы проекта.

---
+++ Lab13/Building.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab13
+{
+    internal class Building
+    {
+        public string Address { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public Building()
+        {
+
+        }
+        public Building(string a, double l, double w, double h)
+        {
+            Address = a;
+            Length = l;
+            Width = w;
+            Height = h;
+        }
+        public string Print()
+        {
+            MultiBuilding multiBuilding = new MultiBuilding("Космическа 13", 2, 3, 4, 5);
+            return ($"{Address}\n{Length}\n{Width}\n{Height}\n{multiBuilding.Floor}");
+        }
+    }
+    sealed class MultiBuilding : Building
+    {
+        public int Floor { get; set; }// добавляет параметр этажности
+        public MultiBuilding(string a, double l, double w, double h, int f)
+        {
+            Floor = f;
+        }
+    }
+}
---
+++ Lab13/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            //RegulTreugolnik regulTreugolnik = new RegulTreugolnik();
+            //Treugolnik treugolnik = regulTreugolnik;
+            //if (treugolnik is PrimTreugolnik)
+            //{
+            //    PrimTreugolnik primtreugolnik = (PrimTreugolnik)treugolnik;
+            //}
+
+            ////PrimTreugolnik primtreugolnik = new PrimTreugolnik(1,2);
+            ////Treugolnik treugolnik = primtreugolnik;
+            ////PrimTreugolnik primTreugolnik = (PrimTreugolnik)treugolnik;
+            //Console.WriteLine(treugolnik.GetSquare());
+            #endregion
+            Building building = new Building("Космическа 13",2,3,4);
+            Console.WriteLine(building.Print());
+            Console.ReadKey();
+        }
+    }
+    #region
+    //class Treugolnik
+    //{
+    //    public double A { get; set; }
+    //    public double B { get; set; }
+    //    public double C { get; set; }
+    //    public Treugolnik()
+    //    {
+
+    //    }
+    //    public Treugolnik(double a, double b, double c)
+    //    {
+    //        A = a;
+    //        B = b;
+    //        C = c;
+    //    }
+
+    //    public double GetSquare()
+    //    {
+    //        double p = (A + B + C) / 2;
+    //        return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+    //    }
+    //}
+    //class PrimTreugolnik : Treugolnik
+    //{
+    //    public PrimTreugolnik(double a, double b)
+    //        : base(a, b, Math.Sqrt(a * a + b * b))
+    //    {
+
+    //    }
+    //}
+    //class RegulTreugolnik : Treugolnik
+    //{
+
+    //}
+    #endregion
+}
